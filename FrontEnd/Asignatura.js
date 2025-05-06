e9440959e359cb4ef9630647dd0f0b60b603250f
Juanc4str1ll0n <juancastrillon868@gmail.com>
2025-05-06 12:13:33 -0500

    Cambios en JS

---
+++ FrontEnd/Asignatura.js
@@ -0,0 +1,8 @@
+let raw = JSON.stringify({
+    "nombreAsig": document.getElementById("nombreAsig").value,
+    "grupoAsig": document.getElementById("grupoAsig").value,
+    "codigoAsig": document.getElementById("codigoAsig").value,
+    "semestreAsig": document.getElementById("semestreAsig").value,
+    "creditosAsig": document.getElementById("creditosAsig").value,
+    
+});
--- FrontEnd/Estudiante.js
+++ FrontEnd/Estudiante.js
@@ -1,20 +1,42 @@
 //JUAN DAVID CASTRILLON Y DIEGO MIGUEL SANCHEZ
-
+//funcion 1
 function guardarEst(){
-    let nombres = document.getElementById("nombres").value;
-    let tipoId = document.getElementById("tipoId").value;
-    let numId = document.getElementById("numeroId").value;
+    
+    const myHeader = new Headers();
+    myHeader.append("Content-Type", "application/json");
+    event.preventDefault();
+    
     let raw = JSON.stringify({
-        "nombres": nombres,
-        "tipoId": tipoId,
-        "numId": numId
+        "nombres": document.getElementById("nombres").value,
+        "tipoId": document.getElementById("tipoId").value,
+        "numId": document.getElementById("numeroId").value
     });
+
+    let requestOptions = {
+        method: 'POST',
+        headers: myHeader,
+        body: raw,
+        redirect: 'follow'
+    };
+
+    fetch("URL", requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
 }
 
+
+//funcion 2
 function modificarEst(){
-    let numId = document.getElementById("numeroId").value;
-    let nuevoNombre = document.getElementById("nuevoNombre").value;
-    let nuevoTipoId = document.getElementById("nuevoTipo").value;
+
+    let raw2 = JSON.stringify({
+        "nuevoNombre": document.getElementById("nuevoNombre").value,
+        "nuevoTipoId": document.getElementById("nuevoTipo").value,
+        "numId": document.getElementById("numeroId").value
+    });
+
+   
 }
 
 
+// ller los datos del html, transformarlos y enviarlos al backend
